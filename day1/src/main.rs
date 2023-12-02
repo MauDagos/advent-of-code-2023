81a05c66034dff03830f741b0c44fda1b44b5041
Mauro D'Agostino <mdagostino@ravenpack.com>
2023-12-02 17:44:25 +0100

    Refactor day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,6 @@
+use std::error;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 const DAY_1_DIGITS: &'static [&str] = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -8,55 +9,42 @@ const DAY_2_DIGITS: &'static [&str] = &[
     "seven", "eight", "nine",
 ];
 
-fn main() {
+fn main() -> Result<(), Box<dyn error::Error>> {
     println!(
         "[Day 1] The sum of all of the calibration values is: {}",
-        sum_calibration_values("./data/input.txt", DAY_1_DIGITS)
+        sum_calibration_values("./data/input.txt", DAY_1_DIGITS)?
     );
     println!(
         "[Day 2] The sum of all of the calibration values is: {}",
-        sum_calibration_values("./data/input.txt", DAY_2_DIGITS)
+        sum_calibration_values("./data/input.txt", DAY_2_DIGITS)?
     );
+    Ok(())
 }
 
-fn sum_calibration_values<P>(filename: P, digits: &[&str]) -> u32
+fn sum_calibration_values<P>(filename: P, digits: &[&str]) -> Result<u32, Box<dyn error::Error>>
 where
     P: AsRef<Path>,
 {
     let mut sum = 0;
-    // File hosts.txt must exist in the current path
-    if let Ok(lines) = read_lines(filename) {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            if let Ok(line) = line {
-                if let Some(value) = calibration_value(line, digits) {
-                    sum += value;
-                }
+    for line in BufReader::new(File::open(filename)?).lines() {
+        if let Ok(line) = line {
+            if let Some(value) = calibration_value(line, digits) {
+                sum += value;
             }
         }
     }
-    sum
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(sum)
 }
 
 fn calibration_value(line: String, digits: &[&str]) -> Option<u32> {
     // Get first digit from `line`
     let mut first_digit = None;
     let mut first_digit_position = None;
-    for p in digits {
-        if let Some(pos) = line.find(p) {
+    for digit in digits {
+        if let Some(pos) = line.find(digit) {
             if first_digit_position == None || pos < first_digit_position? {
                 first_digit_position = Some(pos);
-                first_digit = Some(p);
+                first_digit = Some(digit);
             }
         }
     }
@@ -67,11 +55,11 @@ fn calibration_value(line: String, digits: &[&str]) -> Option<u32> {
     // Get the last digit from `line`
     let mut last_digit = first_digit;
     let mut last_digit_position = None;
-    for p in digits {
-        if let Some(pos) = line.rfind(p) {
+    for digit in digits {
+        if let Some(pos) = line.rfind(digit) {
             if last_digit_position == None || pos > last_digit_position? {
                 last_digit_position = Some(pos);
-                last_digit = Some(p);
+                last_digit = Some(digit);
             }
         }
     }
@@ -108,33 +96,29 @@ mod tests {
 
     #[test]
     fn test_example_1() {
-        assert_eq!(
-            sum_calibration_values("./data/example1.txt", DAY_1_DIGITS),
-            142
-        );
+        if let Ok(result) = sum_calibration_values("./data/example1.txt", DAY_1_DIGITS) {
+            assert_eq!(result, 142)
+        }
     }
 
     #[test]
     fn test_example_2() {
-        assert_eq!(
-            sum_calibration_values("./data/example2.txt", DAY_2_DIGITS),
-            281
-        );
+        if let Ok(result) = sum_calibration_values("./data/example2.txt", DAY_2_DIGITS) {
+            assert_eq!(result, 281);
+        }
     }
 
     #[test]
     fn test_day_1() {
-        assert_eq!(
-            sum_calibration_values("./data/input.txt", DAY_1_DIGITS),
-            55130
-        );
+        if let Ok(result) = sum_calibration_values("./data/input.txt", DAY_1_DIGITS) {
+            assert_eq!(result, 55130);
+        }
     }
 
     #[test]
     fn test_day_2() {
-        assert_eq!(
-            sum_calibration_values("./data/input.txt", DAY_2_DIGITS),
-            54985
-        );
+        if let Ok(result) = sum_calibration_values("./data/input.txt", DAY_2_DIGITS) {
+            assert_eq!(result, 54985);
+        }
     }
 }
